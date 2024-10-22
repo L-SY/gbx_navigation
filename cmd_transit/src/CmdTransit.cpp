13c3cbfe6d792ef5c8fd26c8834f536a1ef02a36
Kai0139 <zkrs3914@outlook.com>
2024-10-22 17:33:22 +0800

    The most important is costmap size.

--- cmd_transit/src/CmdTransit.cpp
+++ cmd_transit/src/CmdTransit.cpp
@@ -11,9 +11,8 @@ public:
   {
     ros::NodeHandle private_nh("~");
     private_nh.param("input_topic", input_topic_, std::string("/cmd_vel"));
-    private_nh.param("output_topic", output_topic_, std::string("/cmd_vel_repeated"));
+    private_nh.param("output_topic", output_topic_, std::string("/ranger/cmd_vel"));
     private_nh.param("frequency", frequency_, 10.0);
-    private_nh.param("timeout", timeout_, 1.0);  // 新增超时时间参数
 
     cmd_vel_sub_ = nh_.subscribe(input_topic_, 10, &CmdTransit::cmdVelCallback, this);
     cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>(output_topic_, 10);
@@ -21,52 +20,20 @@ public:
     timer_ = nh_.createTimer(ros::Duration(1.0 / frequency_), &CmdTransit::timerCallback, this);
 
     has_received_msg_ = false;
-    zero_cmd_count_ = 0;
-    last_msg_time_ = ros::Time::now();
   }
 
 private:
   void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg)
   {
-    last_msg_time_ = ros::Time::now();
-
-    if (msg->linear.x == 0.0 && msg->angular.z == 0.0)
-    {
-      zero_cmd_count_++;
-    }
-    else
-    {
-      zero_cmd_count_ = 0;
-    }
-
     last_cmd_vel_ = *msg;
     has_received_msg_ = true;
   }
 
   void timerCallback(const ros::TimerEvent&)
   {
-    ros::Time current_time = ros::Time::now();
-    if ((current_time - last_msg_time_).toSec() > timeout_)
+    if (has_received_msg_)
     {
-      geometry_msgs::Twist zero_msg;
-      zero_msg.linear.x = 0.0;
-      zero_msg.angular.z = 0.0;
-      cmd_vel_pub_.publish(zero_msg);
-      zero_cmd_count_ = 0;
-    }
-    else if (has_received_msg_)
-    {
-      if (zero_cmd_count_ < 2)
-      {
-        cmd_vel_pub_.publish(last_cmd_vel_);
-      }
-      else
-      {
-        geometry_msgs::Twist zero_msg;
-        zero_msg.linear.x = 0.0;
-        zero_msg.angular.z = 0.0;
-        cmd_vel_pub_.publish(zero_msg);
-      }
+      cmd_vel_pub_.publish(last_cmd_vel_);
     }
   }
 
@@ -77,13 +44,10 @@ private:
 
   geometry_msgs::Twist last_cmd_vel_;
   bool has_received_msg_;
-  int zero_cmd_count_;
-  ros::Time last_msg_time_;
 
   std::string input_topic_;
   std::string output_topic_;
   double frequency_;
-  double timeout_;
 };
 
 int main(int argc, char** argv)
