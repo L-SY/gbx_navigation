e7c40eeae4f293c5f92170d4f6d2e3ac72992437
LSY <2028238109@qq.com>
2024-11-05 14:29:28 +0800

    Add basic gbx_manual.

---
+++ gbx_manual/include/gbx_manual/GBXManual.h
@@ -0,0 +1,10 @@
+//
+// Created by lsy on 24-11-5.
+//
+
+#pragma once
+
+namespace gbx_manual
+{
+
+} // namespace gbx_manual
---
+++ gbx_manual/src/GBXManual.cpp
@@ -0,0 +1,9 @@
+//
+// Created by lsy on 24-11-5.
+//
+
+#include "gbx_manual/GBXManual.h"
+namespace gbx_manual
+{
+
+} // namespace gbx_manual
