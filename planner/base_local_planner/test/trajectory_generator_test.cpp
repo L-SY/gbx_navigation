4e5334d11d8dfbac1ed477f662f13452b0440c19
LSY <2028238109@qq.com>
2024-11-13 15:02:16 +0800

    Add base_local_planner from ros_navigation.

---
+++ planner/base_local_planner/test/trajectory_generator_test.cpp
@@ -0,0 +1,30 @@
+/*
+ * footprint_helper_test.cpp
+ *
+ *  Created on: May 2, 2012
+ *      Author: tkruse
+ */
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include <base_local_planner/simple_trajectory_generator.h>
+
+namespace base_local_planner
+{
+class TrajectoryGeneratorTest : public testing::Test
+{
+public:
+  SimpleTrajectoryGenerator tg;
+
+  TrajectoryGeneratorTest()
+  {
+  }
+
+  virtual void TestBody()
+  {
+  }
+};
+
+}  // namespace base_local_planner
