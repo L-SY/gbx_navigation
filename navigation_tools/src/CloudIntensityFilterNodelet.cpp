16624ad5ef39ca49c45c42feb00de48abf9c87db
LSY <2028238109@qq.com>
2024-12-01 15:26:00 +0800

    Fix intensity err.

--- navigation_tools/src/CloudIntensityFilterNodelet.cpp
+++ navigation_tools/src/CloudIntensityFilterNodelet.cpp
@@ -2,11 +2,6 @@
 // Created by lsy on 24-11-29.
 //
 
-/**
-* @file IntensityFilterNodelet.cpp
-* @brief A nodelet to filter point cloud based on intensity
-*/
-
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
@@ -20,56 +15,92 @@ namespace cloud_filter {
 
 class IntensityFilterNodelet : public nodelet::Nodelet {
 private:
- ros::Publisher pub_;
- ros::Subscriber sub_;
- double intensity_threshold_;
- std::string input_topic_;
- std::string output_topic_;
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
+  double intensity_threshold_;
+  std::string input_topic_;
+  std::string output_topic_;
+  std::string intensity_field_;
 
- virtual void onInit() override {
-   auto& nh = getNodeHandle();
-   auto& private_nh = getPrivateNodeHandle();
+  virtual void onInit() override {
+    auto& nh = getNodeHandle();
+    auto& private_nh = getPrivateNodeHandle();
 
-   intensity_threshold_ = private_nh.param("intensity_threshold", 100.0);
-   input_topic_ = private_nh.param<std::string>("input_topic", "points");
-   output_topic_ = private_nh.param<std::string>("output_topic", "points_filtered");
+    intensity_threshold_ = private_nh.param("intensity_threshold", 100.0);
+    input_topic_ = private_nh.param<std::string>("input_topic", "points");
+    output_topic_ = private_nh.param<std::string>("output_topic", "points_filtered");
+    intensity_field_ = private_nh.param<std::string>("intensity_field", "intensity");
 
-   pub_ = nh.advertise<sensor_msgs::PointCloud2>(output_topic_, 10);
-   sub_ = nh.subscribe<sensor_msgs::PointCloud2>(
-       input_topic_, 10, &IntensityFilterNodelet::cloudCallback, this);
+    pub_ = nh.advertise<sensor_msgs::PointCloud2>(output_topic_, 10);
+    sub_ = nh.subscribe<sensor_msgs::PointCloud2>(
+        input_topic_, 10, &IntensityFilterNodelet::cloudCallback, this);
 
-   NODELET_INFO("Intensity filter nodelet initialized:");
-   NODELET_INFO("  - Input topic: %s", input_topic_.c_str());
-   NODELET_INFO("  - Output topic: %s", output_topic_.c_str());
-   NODELET_INFO("  - Intensity threshold: %f", intensity_threshold_);
- }
+    NODELET_INFO("Intensity filter nodelet initialized:");
+    NODELET_INFO(" - Input topic: %s", input_topic_.c_str());
+    NODELET_INFO(" - Output topic: %s", output_topic_.c_str());
+    NODELET_INFO(" - Intensity threshold: %f", intensity_threshold_);
+    NODELET_INFO(" - Intensity field name: %s", intensity_field_.c_str());
+  }
 
- void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& input) {
-   pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
-   pcl::fromROSMsg(*input, *cloud);
+  void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& input) {
+    sensor_msgs::PointCloud2 output;
 
-   pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
-   cloud_filtered->reserve(cloud->size());
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*input, pcl_pc2);
 
-   for (const auto& point : *cloud) {
-     if (point.intensity >= intensity_threshold_) {
-       cloud_filtered->push_back(point);
-     }
-   }
+    pcl::PCLPointCloud2 filtered_pcl_pc2;
 
-   cloud_filtered->header = cloud->header;
-   cloud_filtered->height = 1;
-   cloud_filtered->width = cloud_filtered->size();
-   cloud_filtered->is_dense = false;
+    int intensity_idx = pcl::getFieldIndex(pcl_pc2, intensity_field_);
+    if (intensity_idx == -1) {
+      NODELET_WARN_THROTTLE(1, "No %s field found in point cloud", intensity_field_.c_str());
+      return;
+    }
 
-   sensor_msgs::PointCloud2 output;
-   pcl::toROSMsg(*cloud_filtered, output);
-   output.header = input->header;
+    std::vector<pcl::PCLPointField> fields = pcl_pc2.fields;
+    size_t point_step = pcl_pc2.point_step;
+    size_t num_points = pcl_pc2.width * pcl_pc2.height;
 
-   pub_.publish(output);
- }
+    filtered_pcl_pc2.fields = fields;
+    filtered_pcl_pc2.point_step = point_step;
+    filtered_pcl_pc2.height = 1;
+    filtered_pcl_pc2.header = pcl_pc2.header;
+    filtered_pcl_pc2.is_bigendian = pcl_pc2.is_bigendian;
+
+    filtered_pcl_pc2.data.reserve(pcl_pc2.data.size());
+
+    try {
+      for (size_t i = 0; i < num_points; ++i) {
+        float intensity;
+        memcpy(&intensity,
+               &pcl_pc2.data[i * point_step + fields[intensity_idx].offset],
+               sizeof(float));
+
+        if (intensity >= intensity_threshold_) {
+          filtered_pcl_pc2.data.insert(
+              filtered_pcl_pc2.data.end(),
+              pcl_pc2.data.begin() + i * point_step,
+              pcl_pc2.data.begin() + (i + 1) * point_step
+          );
+        }
+      }
+
+      filtered_pcl_pc2.width = filtered_pcl_pc2.data.size() / point_step;
+      filtered_pcl_pc2.row_step = filtered_pcl_pc2.width * point_step;
+
+      pcl_conversions::fromPCL(filtered_pcl_pc2, output);
+      output.header = input->header;
+
+      pub_.publish(output);
+
+      NODELET_DEBUG("Filtered cloud: %zu points (input: %zu points)",
+                    filtered_pcl_pc2.width, num_points);
+    }
+    catch (const std::exception& e) {
+      NODELET_ERROR("Error processing point cloud: %s", e.what());
+    }
+  }
 };
 
-}  // namespace cloud_filter
+} // namespace cloud_filter
 
 PLUGINLIB_EXPORT_CLASS(cloud_filter::IntensityFilterNodelet, nodelet::Nodelet)
